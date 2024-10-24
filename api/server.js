35f49d7dbb01247fc14e764eb2999517e566b8cf
Jarliev Perez <55269430+Thotsuya@users.noreply.github.com>
2024-10-23 23:10:44 -0600

    Initial Commit

--- api/server.js
+++ api/server.js
@@ -28,7 +28,7 @@ app.get('*', (req, res) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
